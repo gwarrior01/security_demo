bc7a63f06ae33252fc0e25633dbde9bbfcd0cd43
Alex First <gwarrior01@mail.ru>
2022-08-27 20:59:09 +0300

    dao complete

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -18,69 +18,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userService;
 
-//    private final AuthProviderImpl authProvider;
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(authProvider);
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
-                .antMatchers("/", "/auth/login", "/auth/registration", "/error").permitAll()
-                .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/auth/login")
-                .loginProcessingUrl("/process_login")
-                .failureForwardUrl("/auth/login?error")
-//                .defaultSuccessUrl("")
-                .and()
-                .logout().logoutSuccessUrl("/");
+                .httpBasic();
     }
-
-//    In-Memory
-//    @Bean
-//    public UserDetailsService users(){
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER", "ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-//    JDBC-authentication
-//    @Bean
-//    public JdbcUserDetailsManager users(DataSource dataSource) {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER", "ADMIN")
-//                .build();
-//
-//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-//        users.createUser(user);
-//        users.createUser(admin);
-//        return users;
-//    }
-
-//    DAO Authentication
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder) {
@@ -91,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder PasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,4 @@
---Custom AuthProvider
---insert into person (username, birth_year, password) values ('test_user1', 1995, 'password');
-
---DAO-authentication
 insert into roles (name) values ('ROLE_USER'), ('ROLE_ADMIN');
 insert into users (username, password, email) values
-('user', '$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u', 'test@test.com');
+('user', '$2a$12$0U9GRmh8/fC.Q3M9kNEPQeBjHCG9Izld2woffGJNFxSpCxbMYbrRy', 'test@test.com');
 insert into users_roles (user_id, role_id) values (1, 2);
