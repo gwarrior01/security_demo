db75cf42c65608fb244f5fbe1d59117ec1ecc5f7
Alex First <gwarrior01@mail.ru>
2022-08-27 20:38:29 +0300

    in memory complete

--- Makefile
+++
@@ -1,5 +0,0 @@
-build:
-	docker build -t security_demo_postgres .
-
-run:
-	docker run --name security_demo_postgres --rm -d -p 5432:5432 security_demo_postgres
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,95 +3,48 @@ package com.example.demo.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userService;
-
-//    private final AuthProviderImpl authProvider;
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(authProvider);
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
-                .antMatchers("/", "/auth/login", "/auth/registration", "/error").permitAll()
-                .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/auth/login")
-                .loginProcessingUrl("/process_login")
-                .failureForwardUrl("/auth/login?error")
-//                .defaultSuccessUrl("")
-                .and()
-                .logout().logoutSuccessUrl("/");
-    }
-
-//    In-Memory
-//    @Bean
-//    public UserDetailsService users(){
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER", "ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-//    JDBC-authentication
-//    @Bean
-//    public JdbcUserDetailsManager users(DataSource dataSource) {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{bcrypt}$2a$12$CBwR64JvVCS/47QgtebvLunJlJn4wvmvjcRSWVkaqmvuncGxSja1u")
-//                .roles("USER", "ADMIN")
-//                .build();
-//
-//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-//        users.createUser(user);
-//        users.createUser(admin);
-//        return users;
-//    }
-
-//    DAO Authentication
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-        authenticationProvider.setUserDetailsService(userService);
-        return authenticationProvider;
+                .httpBasic();
     }
 
     @Bean
-    public PasswordEncoder PasswordEncoder() {
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("$2a$12$0U9GRmh8/fC.Q3M9kNEPQeBjHCG9Izld2woffGJNFxSpCxbMYbrRy")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("$2a$12$WxD.MfyDXu44m24PjwEy2e7BJbk71ioMH7D7k/PqYUcdO4S3PTz8u")
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
